4b173f316ecbbc27f84a981f355bd735e82dfe2f
Simone Aucello <152818802+simoneaucello@users.noreply.github.com>
2024-05-16 14:22:08 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container my-5">
+        <h2 class="text-center">Home</h2>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="my-5 text-center text-bg-dark py-5">
+    <h5>Footer</h5>
+</footer>
