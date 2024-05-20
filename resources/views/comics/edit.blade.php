d1e2f453c1a88c29b44913305b1ecb269cd6ec79
simoneaucello <simoneaucello@gmail.com>
2024-05-20 13:57:43 +0200

    [add] print (edit) error messages

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -7,6 +7,19 @@
 @section('content')
     <div class="container my-5">
         <h1 class="text-white">Modifica Fumetto:</h1>
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>
+                            {{ $error }}
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         <h1 class="text-warning mb-5">{{ $comic->title }}</h1>
         <form action="{{ route('comics.update', $comic) }}" method="POST">
             @csrf
@@ -14,34 +27,74 @@
 
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="title" class="form-label">Titolo</label>
-                <input name="title" type="text" class="form-control" id="title" value=" {{ $comic->title }} ">
+                <input name="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                    value=" {{ old('title', $comic->title) }} ">
+
+                @error('title')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="description" class="form-label">Descrizione</label>
-                <input name="description" type="text" class="form-control" id="description"
-                    value=" {{ $comic->description }} " aria-describedby="emailHelp">
+                <input name="description" type="text" class="form-control @error('description') is-invalid @enderror"
+                    id="description" aria-describedby="emailHelp" value=" {{ old('description', $comic->description) }} ">
+
+                @error('description')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="thumb" class="form-label">Immagine(url)</label>
-                <input name="thumb" type="text" class="form-control" id="thumb"
-                    value=" {{ $comic->thumb }} "aria-describedby="emailHelp">
+                <input name="thumb" type="text" class="form-control @error('thumb') is-invalid @enderror"
+                    id="thumb" aria-describedby="emailHelp" value=" {{ old('thumb', $comic->thumb) }} ">
+
+                @error('thumb')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="price" class="form-label">Prezzo</label>
-                <input name="price" type="text" class="form-control" id="price"
-                    value=" {{ $comic->price }} "aria-describedby="emailHelp">
+                <input name="price" type="text" class="form-control @error('price') is-invalid @enderror"
+                    id="price" aria-describedby="emailHelp" value=" {{ old('price', $comic->price) }} ">
+
+                @error('price')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="series" class="form-label">Serie</label>
-                <input name="series" type="text" class="form-control" id="series"
-                    value=" {{ $comic->series }} "aria-describedby="emailHelp">
+                <input name="series" type="text" class="form-control @error('series') is-invalid @enderror"
+                    id="series" aria-describedby="emailHelp" value=" {{ old('series', $comic->series) }} ">
+
+                @error('series')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
+
+
             </div>
             <div class="mb-3 bg-body-tertiary rounded p-2">
                 <label for="sale_date" class="form-label">Data di uscita</label>
-                <input name="sale_date" type="text" class="form-control" id="sale_date"
-                    value=" {{ $comic->sale_date }} "aria-describedby="emailHelp">
+                <input name="sale_date" type="text" class="form-control @error('sale_date') is-invalid @enderror"
+                    id="sale_date" aria-describedby="emailHelp" value=" {{ old('sale_date', $comic->sale_date) }} ">
+
+                @error('sale_date')
+                    <small class="text-danger fw-bold">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <button type="submit" class="btn bg-dark text-white">INSERISCI</button>
+
 
         </form>
     </div>
